daf7afbe977ae0a8f1dd4911127bc542b98e9fe8
PARTH CHAWLA <parthchawla65@gmail.com>
2022-03-24 19:16:46 +0300

    Search bar working

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import CardList from './CardList';
 import { Robots } from './robots';
 import Searchbox from './Searchbox';
 
-const App = () => {
-	return(
-		<div className="tc">
-			<h1>RoboFriends</h1>
-			<Searchbox />
-			<CardList Robots = { Robots } />
-		</div>
-	);
+
+class App extends React.Component{
+	constructor(){
+		super();
+		this.state = {
+			robots: Robots,
+			searchfield: '',
+		}
+	}
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value });
+	}
+	render() {
+		const filteredRobots = this.state.robots.filter(robots => {
+		return robots.name.toLowerCase().startsWith(this.state.searchfield.toLowerCase())
+		})
+		return(
+			<div className="tc">
+				<h1>RoboFriends</h1>
+				<Searchbox SearchChange = {this.onSearchChange}/>
+				<CardList Robots = { filteredRobots } />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/Searchbox.js
+++ src/Searchbox.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Search = () => {
+const Search = ({ SearchChange }) => {
 	return(
 		<div>
-			<input className = "pa3 ba b--green bg-lightest-blue"placeholder = "enter the name" type="Search" />
+			<input className = "pa3 ba b--green bg-lightest-blue"placeholder = "enter the name" type="Search" onChange = {SearchChange}/>
 		</div>
 	);
 }
